4c25245f08d287615b754c0b25c8e3e47714804c
Ondrej89 <ondrejdudas89@gmail.com>
2023-07-17 14:11:45 +0200

    ...

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -24,7 +24,7 @@ const Project = g.model('Project', {
   rules.public().read(),
   rules.private().create().delete().update();
 })
-
+// @ts-ignore
 const jwt = auth.JWT({
   issuer: 'grafbase',
   secret: g.env('NEXTAUTH_SECRET'),
